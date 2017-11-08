7e79233c03eb613fdc8af07e09f92cd7090cb4d2
Rafael Silvestre Santos <rafael.silvestre@conectt.com.br>
2017-11-08 15:08:19 -0200

    Exemplo simples de formulário com envio de dados

--- src/app/login/cadastro/cadastro.component.ts
+++ src/app/login/cadastro/cadastro.component.ts
@@ -1,10 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 
-// {{servico}}
 import { LoginService } from './../login.service';
 
-// {{webservice rest}}
-import { HttpClient } from '@angular/common/http';
+import { FormBuilder, Validators } from '@angular/forms';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
@@ -13,33 +12,38 @@ import { HttpClient } from '@angular/common/http';
 })
 export class CadastroComponent implements OnInit {
 
+  constructor(private loginService: LoginService, public fb: FormBuilder) { }
+
+  public loginForm = new FormGroup({
+    email: new FormControl("email", Validators.required),
+    password: new FormControl("password", Validators.required)
+  });
+
+  doLogin(event) {
+
+    // Show the value of the form
+    let formData = this.loginForm.value;
+    console.log(formData);
+
+    // Or, grab the value of one control:
+    let email = this.loginForm.controls.email.value;
+    console.log(email);
+
+    
+
+    
+    
+
+    console.log(event);
+  }
+
   title: string;
 
-  // {{servico}} // {{webservice rest}}
-  constructor(private loginService: LoginService, private http: HttpClient) { }
+  
+
 
   ngOnInit() {
-
     this.title = this.loginService.someMethod();
-
-    interface UserResponse {
-      login: string;
-      bio: string;
-      company: string;
-    }
-
-    // {{webservice rest}}
-    this.http.get('https://api.github.com/users/seeschweiler').subscribe(data => {
-      console.log(data);
-
-      this.http.get<UserResponse>('https://api.github.com/users/seeschweiler').subscribe(data => {
-        console.log("User Login: " + data.login);
-        console.log("Bio: " + data.bio);
-        console.log("Company: " + data.company);
-      });
-
-      
-    });
   }
 
 }
