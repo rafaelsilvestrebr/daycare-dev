595e4383aa89414b2948342818d8e90701f4c90b
Rafael <rafaelsilvestre.br@gmail.com>
2017-11-27 23:31:29 -0200

    Validação de formulário simples com reactive

--- src/app/login/cadastro/cadastro.component.ts
+++ src/app/login/cadastro/cadastro.component.ts
@@ -2,25 +2,65 @@ import { Component, OnInit } from '@angular/core';
 
 import { LoginService } from './../login.service';
 
-import { FormBuilder, Validators } from '@angular/forms';
-import { FormGroup, FormControl } from '@angular/forms';
+import { FormBuilder, Validators, FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-cadastro',
   templateUrl: './cadastro.component.html',
   styleUrls: ['./cadastro.component.css']
 })
-export class CadastroComponent implements OnInit {
+export class CadastroComponent{
 
-  constructor(private loginService: LoginService, public fb: FormBuilder) { }
+  /////////////////// Form cadastro
+  registerUserForm: FormGroup;
+  post: any;
+  email: string = '';
+  password: string = '';
 
-  public loginForm = new FormGroup({
-    email: new FormControl("email", Validators.required),
-    password: new FormControl("password", Validators.required)
+  titleAlert: string = 'This field is required';
+
+  constructor(private loginService: LoginService, public fb: FormBuilder) { 
+
+    this.registerUserForm = fb.group({
+      'nome' : [null, Validators.required],
+      'email' : [null, Validators.compose([Validators.required, Validators.pattern (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)])],
+      'password' : [null,  Validators.compose([Validators.required, Validators.minLength(6), Validators.maxLength(18)])]
+    });
+
+  }
+
+  addRegister(post) {
+    this.email = post.description;
+    this.password = post.password;
+
+    let formData = this.registerUserForm.value;
+    console.log(formData);
+
+    // Or, grab the value of one control:
+    let nomeVal = this.registerUserForm.controls.nome.value;
+    let emailVal = this.registerUserForm.controls.email.value;
+    let passwordVal = this.registerUserForm.controls.password.value;
+    console.log('Nome = ' + nomeVal);
+    console.log('E-mail = ' + emailVal);
+    console.log('Password = ' + passwordVal);
+    console.log(event);
+  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  /*public loginForm = new FormGroup({
+    email: new FormControl('email', Validators.required),
+    password: new FormControl('password', Validators.required)
   });
 
   doLogin(event) {
-
     // Show the value of the form
     let formData = this.loginForm.value;
     console.log(formData);
@@ -28,22 +68,17 @@ export class CadastroComponent implements OnInit {
     // Or, grab the value of one control:
     let email = this.loginForm.controls.email.value;
     console.log(email);
-
-    
-
-    
-    
-
     console.log(event);
   }
 
   title: string;
 
-  
-
-
   ngOnInit() {
     this.title = this.loginService.someMethod();
   }
+  */
+
+  
+  
 
 }
