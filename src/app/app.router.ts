3a5cab3e1806b664b41bec0e4ee1bfc1c629c6eb
Rafael <rafaelsilvestre.br@gmail.com>
2017-12-19 23:52:53 -0200

    Versão estável do front end, configurado casca inicial e aplicação separada

--- src/app/app.router.ts
+++ src/app/app.router.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { CadastroComponent } from './login/cadastro/cadastro.component';
 import { LoginComponent } from './login/login/login.component';
 import { CadastroEscolaComponent } from './login/cadastro-escola/cadastro-escola.component';
+import { ExemploRestComponent } from './exemplo-rest/exemplo-rest.component';
 
 
 export const router: Routes = [
     { path: '', redirectTo: 'login', pathMatch: 'full'},
     { path: 'login', component: LoginComponent},
     { path: 'cadastro-escola', component: CadastroEscolaComponent},
+    { path: 'exemplo-rest', component: ExemploRestComponent},
     { path: 'cadastro', component: CadastroComponent}
 ];
 
