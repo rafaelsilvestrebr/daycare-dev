6ca44440c94725301b297515a38bcc934bfbece7
Rafael <rafaelsilvestre.br@gmail.com>
2018-02-07 23:01:36 -0200

    Ajuste formulário contato

--- src/app/contato/contato.component.ts
+++ src/app/contato/contato.component.ts
@@ -19,8 +19,10 @@ export class ContatoComponent implements OnInit {
       // tslint:disable-next-line:max-line-length
       'email' : [null, Validators.compose([Validators.required, Validators.pattern (/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)])],
       'firstName' : [null, Validators.required],
+      'ECliente' : [null, Validators.required],
       'departamento' : [null, Validators.required],
-      'telefone' : [null, Validators.compose([Validators.required, Validators.pattern (/^\([0-9]{2}\)[\s][0-9]{4}-[0-9]{4,5}$/)])]
+      'telefone' : [null, Validators.compose([Validators.required, Validators.pattern (/^\([0-9]{2}\)[\s][0-9]{4}-[0-9]{4,5}$/)])],
+      'celular' : [null, Validators.compose([Validators.required, Validators.pattern (/^\([0-9]{2}\)[\s][0-9]{4}-[0-9]{4,5}$/)])]
     });
 
 
